3d4a351ce99754b662b2790ec9dcdbe52966bc78
Andy Maleh <andy.am@gmail.com>
2025-02-19 05:24:04 -0500

    Builder

---
+++ 01_creational_patterns/02_builder/campaign_builder.rb
@@ -0,0 +1,17 @@
+class CampaignBuilder
+  def build_step1
+    # step 1 adds customers to campaign
+  end
+  
+  def build_step2
+    # step 2 edits message to send to customers
+  end
+  
+  def build_step3
+    # step 3 configures campaign send date
+  end
+  
+  def to_campaign
+    # builds final campaign representation
+  end
+end
